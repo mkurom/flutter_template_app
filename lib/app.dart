5156662dbd08046d526fc11427a6356907a31d66
yasudamakoto <hare.v1v1enne@gmail.com>
2022-06-19 03:23:10 +0900

    app.dartにlifecycleProviderを追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:flutter_template_app/life_cycle_provider.dart';
 import 'package:flutter_template_app/providers/firebase_auth/authenticator.dart';
 import 'package:flutter_template_app/presentation/pages/splash/splash_page.dart';
 import 'package:flutter_template_app/presentation/pages/user_info/user_info_page.dart';
@@ -10,6 +11,11 @@ class App extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AppLifecycleState>(
+      appLifecycleProvider,
+      (previous, next) => debugPrint('Previous: $previous, Next: $next'),
+    );
+
     final isSignedIn = ref.watch(isSignedInProvider).value ?? false;
 
     if (!isSignedIn) {
