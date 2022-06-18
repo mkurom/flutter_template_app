035f2437b2888fc730f6956a157b5fb339199cc5
yasudamakoto <hare.v1v1enne@gmail.com>
2022-06-18 16:37:44 +0900

    ライフサイクル追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template_app/presentation/pages/connpass_event/connpass_event_page.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      //
+      print('pause');
+    } else if (state == AppLifecycleState.inactive) {
+      //
+      print('inactive');
+    } else if (state == AppLifecycleState.resumed) {
+      //
+      print('resume');
+    } else if (state == AppLifecycleState.detached) {
+      //
+      print('detecte');
+    }
+
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
