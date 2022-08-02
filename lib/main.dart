94dc48e8473ee979958b1c16ce6fd16ab0ae469e
yasudamakoto <hare.v1v1enne@gmail.com>
2022-08-03 01:02:01 +0900

    [fix]importパッケージ修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter_template_app/app.dart';
