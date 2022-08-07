a13c8cfc04f82fee73c84138d11b914180b648b9
yasudamakoto <hare.v1v1enne@gmail.com>
2022-08-07 18:39:37 +0900

    [add]ボトムナビゲーション作成

---
+++ lib/tab_navigator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TabNavigatorRoutes {
+  static const String root = '/';
+}
+
+class TabNavigator extends StatelessWidget {
+  const TabNavigator({
+    Key? key,
+    required this.navigatorKey,
+    required this.page,
+  }) : super(key: key);
+
+  final GlobalKey<NavigatorState> navigatorKey;
+  final Widget page;
+
+  Map<String, WidgetBuilder> _routeBuilders(BuildContext context) {
+    return {
+      TabNavigatorRoutes.root: (context) => page,
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final routeBuilders = _routeBuilders(context);
+    return Navigator(
+      key: navigatorKey,
+      initialRoute: TabNavigatorRoutes.root,
+      onGenerateRoute: (routeSettings) {
+        final name = routeSettings.name as String;
+        final widget = routeBuilders[name]!;
+        return MaterialPageRoute<StatefulWidget>(
+          builder: widget,
+        );
+      },
+    );
+  }
+}
